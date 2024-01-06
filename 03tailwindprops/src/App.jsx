721094c8f3cbd2b2276423d4a02e9cb4566794cb
BIpin <bipin.regmi.s21@gmail.com>
2024-01-07 00:32:03 +0545

    Basic and Foundation of Props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card"
+
+function App() {
+
+
+  return (
+    <>
+      <Card name='Bipin Regmi' btnText='Visit' />
+      <Card name='Binay Regmi' />
+    </>
+  )
+}
+
+export default App
